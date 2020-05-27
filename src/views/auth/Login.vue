f00a83c8dc2ee5ba3712027613a8cc2a783ad6b1
HiRahmat-Dev <ahmadalhidayah1@gmail.com>
2020-05-27 16:33:44 +0700

    Update

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="login">
-    <div class="btn-login" @click="login">
-      <img src="@/assets/img/svg/chat-doang.svg">
-    </div>
+    <form class="form-login" @submit.prevent="cek">
+      <div class="logo">
+        <img src="@/assets/img/svg/chat-doang.svg">
+      </div>
+      <div class="grup-form">
+        <input type="text" placeholder="Email">
+      </div>
+      <div class="grup-form">
+        <input type="text" placeholder="Password">
+      </div>
+      <button class="btn btn-primary">Login</button>
+      <router-link class="btn btn-secondary" to="/register">Signin</router-link>
+      <p class="text-center">atau</p>
+      <div id="customBtn" @click="login" class="customGPlusSignIn">
+        <img src="@/assets/img/g-normal.png" class="icon"/>
+        <span class="buttonText">Signin with Google</span>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -13,6 +28,9 @@ import firebase from 'firebase'
 export default {
   name: 'Login',
   methods: {
+    cek () {
+      alert('Berhasil')
+    },
     login () {
       var provider = new firebase.auth.GoogleAuthProvider()
       provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
@@ -51,18 +69,85 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.login {
-  width: 100%;
-}
-.btn-login {
-  width: 300px;
-  margin: 100px auto 0;
-  padding: 16px 25px;
+.logo {
+  width: 200px;
+  margin: 0 auto 10px;
   font-size: 20px;
-  cursor: pointer;
   img {
     width: 100%;
     object-fit: cover;
   }
+}
+form.form-login {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.075);
+  width: 400px;
+  margin: 100px auto 0;
+  padding: 20px;
+  border-radius: 10px;
+  input {
+    width: 100%;
+    padding: 10px;
+    font-size: 18px;
+    margin-bottom: 10px;
+    outline: none;
+    border: 1px solid rgb(197, 197, 197);
+  }
+  .btn {
+    display: block;
+    width: 100%;
+    text-align: center;
+    text-decoration: none;
+    color: black;
+    font-size: 18px;
+    padding: 10px;
+    outline: none;
+    border: 1px solid black;
+    cursor: pointer;
+  }
+  .btn-primary {
+    background-color: #98D79C;
+    border: none;
+    color: white;
+    margin-bottom: 10px;
+  }
+  .btn-secondary {
+    background-color: white;
+    border: 1px solid #98D79C;
+    color: #98D79C;
+  }
+}
+input,
+.btn {
+  border-radius: 5px;
+}
+#customBtn {
+  display: inline-block;
+  background: white;
+  color: #444;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.123);
+  white-space: nowrap;
+  padding: 12px;
+  width: 100%;
+}
+#customBtn:hover {
+  cursor: pointer;
+}
+img.icon {
+  display: inline-block;
+  vertical-align: middle;
+  width: 25px;
+  height: 25px;
+}
+span.buttonText {
+  display: inline-block;
+  vertical-align: middle;
+  padding-left: 12px;
+  font-size: 18px;
+  line-height: 1;
+}
+.text-center {
+  padding: 12px 0;
+  text-align: center;
 }
 </style>
